0baf45873a6d3446fd5a51e795ce92bd64351d7c
moferaisreal <marcelovprm@gmail.com>
2025-06-27 11:37:40 -0300

    branch realocation

--- EXERCICIOS/FRONT END/exercicio_html_js/script.js
+++ EXERCICIOS/FRONT END/exercicio_html_js/script.js
@@ -1,22 +1,20 @@
-const numberOne = document.getElementById('numberA');
-const numberTwo = document.getElementById('numberB');
-const form = document.getElementById('formComp');
-const msg = document.getElementById('message');
+const numberOne = document.getElementById("numberA");
+const numberTwo = document.getElementById("numberB");
+const form = document.getElementById("formComp");
+const msg = document.getElementById("message");
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault(); 
-    console.log("Formulário enviado!"); // Debug
+  event.preventDefault();
+  console.log("Formulário enviado!"); // Debug
 
+  const num1 = parseFloat(numberOne.value);
+  const num2 = parseFloat(numberTwo.value);
 
-    const num1 = parseFloat(numberOne.value);
-    const num2 = parseFloat(numberTwo.value);
-
-    if (num2 > num1) {
-        msg.innerHTML = "Sucesso!";
-        msg.style.color = "var(--color2)";
-    } else {
-        msg.innerHTML = "Tente Novamente!";
-        msg.style.color = "var(--color1)";
-
-    }
+  if (num2 > num1) {
+    msg.innerHTML = "Sucesso!";
+    msg.style.color = "var(--color2)";
+  } else {
+    msg.innerHTML = "Tente Novamente!";
+    msg.style.color = "var(--color1)";
+  }
 });
