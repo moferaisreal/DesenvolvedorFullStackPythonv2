aaf0d8b50bf3f643fac86a1dafdb8718433e96f8
moferaisreal <marcelovprm@gmail.com>
2025-08-07 10:37:50 -0300

    finalizado+mobile

--- EXERCICIOS/FRONT END/projeto_04/src/script.js
+++ EXERCICIOS/FRONT END/projeto_04/src/script.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll("[data-tab-button]");
+  const questions = document.querySelectorAll("[data-faq-question]");
+  const heroSection = document.querySelector(".hero");
+  const heroAltura = heroSection.clientHeight;
+
+  //scroll suave para link interno
+  window.addEventListener("scroll", function () {
+    const posicaoAtual = window.scrollY;
+    if (posicaoAtual > heroAltura) {
+      ocultarElementosHeader();
+    } else {
+      exibirElementosHeader();
+    }
+  });
+
+  //abas atrações
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (botao) {
       const abaAlvo = botao.target.dataset.tabButton;
@@ -10,7 +25,18 @@ document.addEventListener("DOMContentLoaded", function () {
       botao.target.classList.add("shows__tabs__button--is-active");
     });
   }
+  //faq accordion
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener("click", abrirOuFecharResposta);
+  }
 });
+
+function abrirOuFecharResposta(elemento) {
+  const classe = "faq__questions__item--is-open";
+  const elementoPai = elemento.target.parentNode;
+  elementoPai.classList.toggle(classe);
+}
+
 function removerBotaoAtivo() {
   const buttons = document.querySelectorAll("[data-tab-button]");
   for (let i = 0; i < buttons.length; i++) {
@@ -23,4 +49,14 @@ function esconderTodasAbas() {
   for (let i = 0; i < tabsContainer.length; i++) {
     tabsContainer[i].classList.remove("shows__list--is-active");
   }
+}
+
+function ocultarElementosHeader() {
+  const header = document.querySelector("header");
+  header.classList.remove("header--is-hidden");
+}
+
+function exibirElementosHeader() {
+  const header = document.querySelector("header");
+  header.classList.add("header--is-hidden");
 }
