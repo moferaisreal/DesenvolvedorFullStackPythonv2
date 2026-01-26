7fbafb8a2c7fcb0bdc77cfab513646e66840ce8f
moferaisreal <marcelovprm@gmail.com>
2026-01-26 09:20:40 -0300

    chores: configurando ambiente

---
+++ MATERIAL/FRONT_END/styled_components/portfolio/src/App.tsx
@@ -0,0 +1,12 @@
+import GlobalStyle from "./styles";
+import Tst from "./tst";
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Tst />;
+    </>
+  );
+}
+
+export default App;
---
+++ MATERIAL/FRONT_END/styled_components/portfolio/src/styles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        background-color: #121214;
+        color: #fff;
+        font-family: 'Roboto', sans-serif;
+    }
+`;
+
+export default GlobalStyle;
